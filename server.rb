1b58a8c52047b51652368a15a2dc8c082b528531
raventid <raventid@users.noreply.github.com>
2015-05-22 09:23:22 +0300

    fix json response

--- server.rb
+++ server.rb
@@ -11,7 +11,7 @@ def get_content_type(path)
     return "text/plain" if ext == ".rb"
     return "text/xml"   if ext == ".xml"
     return "text/xml"   if ext == ".xsl"
-    return "text/plain" if ext == ".json"
+    return "application/json" if ext == ".json"
     return "text/html"
 end
 #write c server
